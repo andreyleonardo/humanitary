ab9cf74d969819ee654d23fabe27c48702759366
Andrey Leonardo <andrey.leonardo@gmail.com>
2015-10-14 19:00:14 -0300

    [FIX]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  
-  
+
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path    
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
-  
+
   post 'humanitary_api/create_group' => 'groups#create_group', controller: 'groups'
   post 'humanitary_api/near_groups' => 'groups#near_groups', controller: 'groups'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
